c6537e795f370d3b95c36207518a55ac94d27fc2
Tomas <tmercadoslp@gmail.com>
2023-01-03 12:06:57 -0300

    chore: add react-icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
           }
         `}
       </style>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
