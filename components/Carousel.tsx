92146336be9f84a7b1874e74acb95470b9b24f73
Tomas <tmercadoslp@gmail.com>
2023-01-04 21:50:16 -0300

    feat(components): add Carousel

---
+++ components/Carousel.tsx
@@ -0,0 +1,56 @@
+import React, { PropsWithChildren } from 'react';
+import Slider, { Settings } from 'react-slick';
+
+import classNames from 'classnames';
+
+const Carousel: React.FC<PropsWithChildren> = ({ children }) => {
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+
+  const settings: Settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    onSwipe: (dir) => {
+      if (dir === 'left') {
+        setCurrentSlide((prev) => {
+          if (prev === React.Children.count(children) - 1) {
+            return 0;
+          }
+
+          return prev + 1;
+        });
+      } else {
+        setCurrentSlide((prev) => {
+          if (prev === 0) {
+            return React.Children.count(children) - 1;
+          }
+
+          return prev - 1;
+        });
+      }
+    },
+    customPaging: (i) => {
+      return (
+        <div className="flex justify-center items-center">
+          <div
+            className={classNames([
+              'w-2 h-2 bg-white rounded-full',
+              {
+                'bg-gray-500': i !== currentSlide,
+              },
+            ])}
+            onClick={() => setCurrentSlide(i)}
+          />
+        </div>
+      );
+    },
+    dotsClass: 'slick-dots !relative !bottom-0',
+  };
+
+  return <Slider {...settings}>{children}</Slider>;
+};
+
+export default Carousel;
