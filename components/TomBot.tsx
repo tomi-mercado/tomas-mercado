0c37cc43d308f2e90bfc4a7e5b53c7155e7af7a6
Tomas <tmercadoslp@gmail.com>
2023-09-09 03:13:41 -0300

    refactor: separate hook useChatbot

--- components/TomBot.tsx
+++ components/TomBot.tsx
@@ -1,3 +1,4 @@
+import useChatbot from 'hooks/useChatbot';
 import remarkGfm from 'remark-gfm';
 
 import React, { useEffect, useState } from 'react';
@@ -6,8 +7,6 @@ import {
   FaPaperPlane as SendIcon,
 } from 'react-icons/fa';
 import ReactMarkdown from 'react-markdown';
-
-import { useUser } from '@auth0/nextjs-auth0/client';
 
 interface TomBotProps {
   description: string;
@@ -62,114 +61,17 @@ const NotIddleWrapper: React.FC<{
 );
 
 const TomBot: React.FC<TomBotProps> = ({ description, placeholder }) => {
-  const [status, setStatus] = useState<TomBotStatus>('iddle');
-  const [questionValue, setQuestionValue] = useState('');
+  const {
+    loadingAuth,
+    status,
+    questionValue,
+    response,
+    isLoginModalOpen,
+    setQuestionValue,
+    getAction,
+  } = useChatbot();
+
   const loadingMessage = useLoadingMessage(status);
-  const [response, setResponse] = useState<string | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const { user, isLoading: loadingAuth } = useUser();
-
-  const openLoginRequiredModal = () => {
-    setIsModalOpen(true);
-    setTimeout(() => {
-      // @ts-expect-error
-      document.getElementById('login-modal')?.showModal();
-    }, 100);
-  };
-
-  const actions = {
-    iddle: {
-      onSubmit: async () => {
-        if (!questionValue) {
-          return;
-        }
-
-        if (!user) {
-          openLoginRequiredModal();
-          return;
-        }
-
-        setStatus('loading');
-
-        try {
-          const r = await fetch('/api/chatbot', {
-            method: 'POST',
-            body: JSON.stringify({ prompt: questionValue }),
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-
-          if (!r.ok) {
-            throw new Error('Error');
-          }
-
-          const data = (await r.json()) as { response: string };
-          actions.loading.onSuccess(data.response);
-        } catch (e) {
-          actions.loading.onError();
-        }
-      },
-    },
-    loading: {
-      onSuccess: (response: string) => {
-        setResponse(response);
-        setStatus('success');
-      },
-      onError: () => {
-        setStatus('error');
-      },
-    },
-    success: {
-      onNewQuestion: () => {
-        setStatus('iddle');
-        setQuestionValue('');
-        setResponse(null);
-      },
-    },
-    error: {
-      onRetry: () => {
-        actions.iddle.onSubmit();
-      },
-    },
-  };
-
-  const getAction = (action: 'submit' | 'again' | 'retry') => {
-    if (action === 'submit') {
-      switch (status) {
-        case 'iddle':
-          return actions.iddle.onSubmit;
-        case 'error':
-          return actions.error.onRetry;
-        default:
-          throw new Error(
-            `Action ${action} is not available on status ${status}`,
-          );
-      }
-    }
-
-    if (action === 'again') {
-      switch (status) {
-        case 'success':
-          return actions.success.onNewQuestion;
-        default:
-          throw new Error(
-            `Action ${action} is not available on status ${status}`,
-          );
-      }
-    }
-
-    if (action === 'retry') {
-      switch (status) {
-        case 'error':
-          return actions.error.onRetry;
-        default:
-          throw new Error(
-            `Action ${action} is not available on status ${status}`,
-          );
-      }
-    }
-  };
 
   return (
     <form
@@ -250,7 +152,7 @@ const TomBot: React.FC<TomBotProps> = ({ description, placeholder }) => {
         </div>
       )}
 
-      {isModalOpen && (
+      {isLoginModalOpen && (
         <dialog id="login-modal" className="modal">
           <div className="modal-box">
             <h3 className="font-bold text-lg">
---
+++ hooks/useChatbot.ts
@@ -0,0 +1,130 @@
+import { useState } from 'react';
+
+import { useUser } from '@auth0/nextjs-auth0/client';
+
+type TomBotStatus = 'iddle' | 'loading' | 'success' | 'error';
+
+const useChatbot = () => {
+  // Chatbot
+  const [status, setStatus] = useState<TomBotStatus>('iddle');
+  const [questionValue, setQuestionValue] = useState('');
+  const [response, setResponse] = useState<string | null>(null);
+
+  // Auth
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const { user, isLoading: loadingAuth } = useUser();
+
+  const openLoginRequiredModal = () => {
+    setIsLoginModalOpen(true);
+    setTimeout(() => {
+      // @ts-expect-error
+      document.getElementById('login-modal')?.showModal();
+    }, 100);
+  };
+
+  const actions = {
+    iddle: {
+      onSubmit: async () => {
+        if (!questionValue) {
+          return;
+        }
+
+        if (!user) {
+          openLoginRequiredModal();
+          return;
+        }
+
+        setStatus('loading');
+
+        try {
+          const r = await fetch('/api/chatbot', {
+            method: 'POST',
+            body: JSON.stringify({ prompt: questionValue }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+
+          if (!r.ok) {
+            throw new Error('Error');
+          }
+
+          const data = (await r.json()) as { response: string };
+          actions.loading.onSuccess(data.response);
+        } catch (e) {
+          actions.loading.onError();
+        }
+      },
+    },
+    loading: {
+      onSuccess: (response: string) => {
+        setResponse(response);
+        setStatus('success');
+      },
+      onError: () => {
+        setStatus('error');
+      },
+    },
+    success: {
+      onNewQuestion: () => {
+        setStatus('iddle');
+        setQuestionValue('');
+        setResponse(null);
+      },
+    },
+    error: {
+      onRetry: () => {
+        actions.iddle.onSubmit();
+      },
+    },
+  };
+
+  const getAction = (action: 'submit' | 'again' | 'retry') => {
+    if (action === 'submit') {
+      switch (status) {
+        case 'iddle':
+          return actions.iddle.onSubmit;
+        case 'error':
+          return actions.error.onRetry;
+        default:
+          throw new Error(
+            `Action ${action} is not available on status ${status}`,
+          );
+      }
+    }
+
+    if (action === 'again') {
+      switch (status) {
+        case 'success':
+          return actions.success.onNewQuestion;
+        default:
+          throw new Error(
+            `Action ${action} is not available on status ${status}`,
+          );
+      }
+    }
+
+    if (action === 'retry') {
+      switch (status) {
+        case 'error':
+          return actions.error.onRetry;
+        default:
+          throw new Error(
+            `Action ${action} is not available on status ${status}`,
+          );
+      }
+    }
+  };
+
+  return {
+    loadingAuth,
+    status,
+    questionValue,
+    response,
+    isLoginModalOpen,
+    setQuestionValue,
+    getAction,
+  };
+};
+
+export default useChatbot;
