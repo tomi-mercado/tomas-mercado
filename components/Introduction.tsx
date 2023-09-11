74b58b65f4908f598d0e34779f386e0250a4400d
Tomas <tmercadoslp@gmail.com>
2023-09-10 23:43:13 -0300

    feat(Introduction): border for main img

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -23,14 +23,16 @@ const Introduction: React.FC = () => {
         {title.iAm} <UnderlinedText>{title.name}</UnderlinedText> 👋🏻
       </h2>
 
-      <Image
-        src={image.src}
-        alt={image.alt}
-        width={150}
-        height={150}
-        priority
-        className="rounded-full w-[150px] h-[150px] object-cover"
-      />
+      <div className="rounded-full w-[160px] h-[160px] bg-secondary relative">
+        <Image
+          src={image.src}
+          alt={image.alt}
+          width={150}
+          height={150}
+          priority
+          className="rounded-full w-[150px] h-[150px] object-cover absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        />
+      </div>
 
       <p className="text-lg">{description}</p>
     </>
