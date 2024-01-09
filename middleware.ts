b3ec1976885373ba94dddcddb1bd5d282fe7e71b
Tomas <tmercadoslp@gmail.com>
2024-01-09 20:05:10 -0300

    u.u

--- middleware.ts
+++ middleware.ts
@@ -22,20 +22,5 @@ export const middleware: NextMiddleware = (request) => {
 };
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)'],
 };
