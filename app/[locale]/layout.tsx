31f90245a3ebab29e49fc808b0b5d09a21c4cec8
Tomas <tmercadoslp@gmail.com>
2024-01-10 20:59:53 -0300

    fix: add lang again to layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,6 +3,31 @@ import Navbar from 'components/Navbar';
 import Providers from 'components/Providers';
 import readContent from 'services/content';
 import { homeSchema } from 'utils/content/homeContentValidation';
+import replaceYearsExperience from 'utils/replaceYearsExperience';
+
+import { Metadata } from 'next';
+import { Lato } from 'next/font/google';
+
+const lato = Lato({
+  subsets: ['latin-ext'],
+  weight: ['400', '700', '900'],
+});
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: 'en' | 'es' };
+}): Promise<Metadata> {
+  return {
+    title: 'Tomás Mercado - Developer',
+    description: replaceYearsExperience(
+      {
+        en: `Website of Tomás Mercado, Full Stack Developer with more than [yearsExperience] years of experience`,
+        es: `Sitio web de Tomás Mercado, Desarrollador Full Stack con más de [yearsExperience] años de experiencia`,
+      }[params.locale],
+    ),
+  };
+}
 
 export default async function RootLayout({
   children,
@@ -16,12 +41,16 @@ export default async function RootLayout({
   const content = await readContent('content/home.json', locale, homeSchema);
 
   return (
-    <Providers content={content} locale={locale}>
-      {/* @ts-expect-error */}
-      <Navbar locale={locale} />
-      <div className="flex flex-col grow pt-[64px]">{children}</div>
-      {/* @ts-expect-error */}
-      <Footer locale={locale} />
-    </Providers>
+    <html data-theme="tomTheme" className={lato.className}>
+      <body className="flex flex-col min-h-screen">
+        <Providers content={content} locale={locale}>
+          {/* @ts-expect-error */}
+          <Navbar locale={locale} />
+          <div className="flex flex-col grow pt-[64px]">{children}</div>
+          {/* @ts-expect-error */}
+          <Footer locale={locale} />
+        </Providers>
+      </body>
+    </html>
   );
 }
