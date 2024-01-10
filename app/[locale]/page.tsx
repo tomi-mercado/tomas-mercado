db32659a9889c5b5f778bfd176e6bb33ece09e1d
Tomas <tmercadoslp@gmail.com>
2024-01-10 12:38:48 -0300

    fix(home): use BlogSection

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import SectionContainer from 'components/SectionContainer';
-import UnderlinedText from 'components/UnderlinedText';
 import { getPosts } from 'services/posts';
 
+import BlogSection from './components/BlogSection';
 import Contact from './components/Contact';
 import Introduction from './components/Introduction';
 import ProjectsSection from './components/ProjectsSection';
@@ -27,16 +27,8 @@ const Home = async ({
       </SectionContainer>
       {/* @ts-expect-error */}
       <ProjectsSection locale={locale} />
-      {postsAmount > 0 && (
-        <SectionContainer className="text-center">
-          <UnderlinedText className="text-3xl">Blog</UnderlinedText>
-          <p>
-            Experiences, thoughts, explanations and other things about my day to
-            day as a developer.
-          </p>
-          <button className="btn btn-primary">Check out my blog</button>
-        </SectionContainer>
-      )}
+      {/* @ts-expect-error */}
+      {postsAmount > 0 && <BlogSection locale={locale} />}
       {/* @ts-expect-error */}
       <Contact locale={locale} />
     </>
