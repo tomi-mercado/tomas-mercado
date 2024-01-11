e48866a70456492a06a57dcd202848d8e71215a9
Tomas <tmercadoslp@gmail.com>
2024-01-10 21:35:36 -0300

    u

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,17 @@
 import LinkMantainLocale from 'components/LinkMantainLocale';
 import MarkedHighlightText from 'components/MarkedHighlightText';
 
+import { Lato } from 'next/font/google';
 import Image from 'next/image';
+
+const lato = Lato({
+  subsets: ['latin-ext'],
+  weight: ['400', '700', '900'],
+});
 
 export default async function NotFound() {
   return (
-    <html lang="en" data-theme="tomTheme">
+    <html lang="en" data-theme="tomTheme" className={lato.className}>
       <body className="flex flex-col min-h-screen">
         <div className="container py-6 flex flex-col gap-4 h-full flex-1">
           <div className="w-full h-full flex flex-col gap-6 items-center flex-1 justify-center">
