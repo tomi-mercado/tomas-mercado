478436f069b779e7d1d4892d5d0806cc493d665f
Tomas <tmercadoslp@gmail.com>
2024-01-10 21:56:38 -0300

    home: update own description

--- app/[locale]/components/Introduction/index.tsx
+++ app/[locale]/components/Introduction/index.tsx
@@ -4,48 +4,11 @@ import { homeSchema } from 'utils/content/homeContentValidation';
 import getTimeOfDay from 'utils/getTimeOfDay';
 import replaceYearsExperience from 'utils/replaceYearsExperience';
 
-import { ReactNode } from 'react';
-import { SiTypescript } from 'react-icons/si';
-
-import Image from 'next/image';
-
 import SwapImage from './SwapImage';
 
 interface IntroductionProps {
   locale: 'en' | 'es';
 }
-
-const replacerDescription = (
-  str: string,
-  toReplace: {
-    strToReplace: string;
-    replacement: ReactNode;
-  }[],
-) => {
-  return (
-    <span>
-      {str.split(' ').map((word, i) => {
-        const replacement = toReplace.find(({ strToReplace }) =>
-          word.includes(strToReplace),
-        );
-
-        if (replacement) {
-          const charsPreviousToLogo = word.split(replacement.strToReplace)[0];
-          const charsAfterLogo = word.split(replacement.strToReplace)[1];
-          return (
-            <>
-              {charsPreviousToLogo}
-              {replacement.replacement}
-              {charsAfterLogo}{' '}
-            </>
-          );
-        }
-
-        return <span key={i}>{word} </span>;
-      })}
-    </span>
-  );
-};
 
 const Introduction = async ({ locale }: IntroductionProps) => {
   const {
@@ -73,28 +36,7 @@ const Introduction = async ({ locale }: IntroductionProps) => {
 
       <SwapImage image={image} />
 
-      <p className="text-lg">
-        {replacerDescription(replaceYearsExperience(description), [
-          {
-            strToReplace: '[quilmesLogo]',
-            replacement: (
-              <Image
-                src="/images/quilmes.png"
-                alt="Quilmes Atletico Club escudo"
-                width={16}
-                height={16}
-                className="inline-block mb-1"
-              />
-            ),
-          },
-          {
-            strToReplace: '[typescriptLogo]',
-            replacement: (
-              <SiTypescript className="inline-block text-blue-600 ml-1 mb-1" />
-            ),
-          },
-        ])}
-      </p>
+      <p className="text-lg">{replaceYearsExperience(description)}</p>
     </>
   );
 };
