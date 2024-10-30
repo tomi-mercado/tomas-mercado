719d7fcf618bcf4f5b6da889a036969b9876eac2
Tomas <tmercadoslp@gmail.com>
2024-10-30 04:31:35 -0300

    change yarn by pnpm, remove daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,50 +18,5 @@ module.exports = {
       },
     },
   },
-  daisyui: {
-    themes: [
-      'dark',
-      {
-        tomTheme: {
-          primary: '#ffffff',
-          'primary-focus': '#e6e6e6',
-          'primary-content': '#000000',
-
-          secondary: '#252530',
-          'secondary-focus': '#091120',
-          'secondary-content': '#ffffff',
-
-          accent: '#412119',
-          'accent-focus': '#32202c',
-          'accent-content': '#ffffff',
-
-          neutral: '#171618',
-          'neutral-focus': '#2e2d2f',
-          'neutral-content': '#ddc297',
-
-          'base-100': '#141414',
-          'base-200': '#171618',
-          'base-300': '#2e2d2f',
-          'base-content': '#b5a997',
-
-          info: '#66c7ff',
-          success: '#87cf3a',
-          warning: '#e1d460',
-          error: '#ff6b6b',
-
-          '--rounded-box': '1rem',
-          '--rounded-btn': '.2rem',
-          '--rounded-badge': '1.9rem',
-
-          '--animation-btn': '.25s',
-          '--animation-input': '.2s',
-
-          '--btn-text-case': 'uppercase',
-          '--navbar-padding': '.5rem',
-          '--border-btn': '1px',
-        },
-      },
-    ],
-  },
-  plugins: [require('daisyui'), require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography')],
 };
