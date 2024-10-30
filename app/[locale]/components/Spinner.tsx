d070e564b30483a79f9eb9698b0754dc921ccfc4
Tomas <tmercadoslp@gmail.com>
2024-10-30 10:30:20 -0300

    add expect error

--- app/[locale]/components/Spinner.tsx
+++ app/[locale]/components/Spinner.tsx
@@ -1,3 +1,4 @@
 export const Spinner = ({ size = 45 }: { size?: number }) => {
+  // @ts-expect-error in the future I'll add the declare for this
   return <l-helix size={size} speed="2.5" color="white" />;
 };
